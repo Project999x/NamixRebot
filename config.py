ef66f08bea835263b8b59f40b5f2d88ead0a6582
Project999x <105520683+Project999x@users.noreply.github.com>
2025-08-08 20:16:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "9016")
 
 #Database --⚠️ REQUIRED--
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kaido0099878:XR5TnmaT55neAJ2U@cluster0.ksifz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "@NamiXRobit_Bot")
+DB_NAME = os.environ.get("DATABASE_NAME", "NamiXRobit_Bot")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
